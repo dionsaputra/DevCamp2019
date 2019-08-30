93ec6b5f2af946733ea2c75ee18135cfe3ce8b34
ridwanakf <blog.afwan@gmail.com>
2019-08-31 04:03:44 +0700

    [Ridwan] added image search

--- Android/app/src/main/java/ds/appname/home/DiscoveryActivity.java
+++ Android/app/src/main/java/ds/appname/home/DiscoveryActivity.java
@@ -1,22 +1,61 @@
 package ds.appname.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.common.modeldownload.FirebaseRemoteModel;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceAutoMLImageLabelerOptions;
+
+import java.util.List;
 
 import ds.appname.R;
 
 public class DiscoveryActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888; // field
+    private static final String TAG = "DiscoveryActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discovery);
+
+        FirebaseApp.initializeApp(this);
+
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        FirebaseRemoteModel remoteModel = new FirebaseRemoteModel.Builder("model")
+                .enableModelUpdates(true)
+                .setInitialDownloadConditions(conditions)
+                .setUpdatesDownloadConditions(conditions)
+                .build();
+        FirebaseModelManager.getInstance().registerRemoteModel(remoteModel);
+
+        FirebaseLocalModel localModel = new FirebaseLocalModel.Builder("model")
+                .setAssetFilePath("manifest.json")
+                .build();
+        FirebaseModelManager.getInstance().registerLocalModel(localModel);
+
+
         takePicture();
+
     }
 
 
@@ -31,7 +70,46 @@ public class DiscoveryActivity extends AppCompatActivity {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap picture = (Bitmap) data.getExtras().get("data");//this is your bitmap image and now you can do whatever you want with this
             //TODO: bitmapnya disini
+            try {
+                inferenceBitmap(picture);
+            } catch (FirebaseMLException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void inferenceBitmap(Bitmap picture) throws FirebaseMLException {
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(picture);
+        FirebaseVisionOnDeviceAutoMLImageLabelerOptions labelerOptions =
+                new FirebaseVisionOnDeviceAutoMLImageLabelerOptions.Builder()
+                        .setLocalModelName("model")    // Skip to not use a local model
+                        .setRemoteModelName("model")  // Skip to not use a remote model
+                        .setConfidenceThreshold(0)  // Evaluate your model in the Firebase console
+                        // to determine an appropriate value.
+                        .build();
+        FirebaseVisionImageLabeler labeler =
+                FirebaseVision.getInstance().getOnDeviceAutoMLImageLabeler(labelerOptions);
+        labeler.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                        for (FirebaseVisionImageLabel label: labels) {
+                            String text = label.getText();
+                            float confidence = label.getConfidence();
+                            Log.d(TAG, "onSuccess: " + text + " : " + confidence);
+                            //TODO: disini labelnya
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+
     }
 
     @Override

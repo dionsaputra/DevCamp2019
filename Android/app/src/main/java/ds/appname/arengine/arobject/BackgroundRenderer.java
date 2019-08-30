362080017be4f68ce9c334b84eb76fc056c37c9f
ridwanakf <blog.afwan@gmail.com>
2019-08-30 19:08:17 +0700

    [Ridwan] added arengine

---
+++ Android/app/src/main/java/ds/appname/arengine/arobject/BackgroundRenderer.java
@@ -0,0 +1,7 @@
+package ds.appname.arengine.arobject;
+
+import com.maxst.ar.TrackedImage;
+
+abstract class BackgroundRenderer extends BaseRenderer {
+	abstract void draw(TrackedImage trackedImage);
+}
